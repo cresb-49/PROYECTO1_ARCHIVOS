c69e9ed06d6276f5527e89f20fb36e18e03dc145
cresb-49 <carlosbpac@gmail.com>
2023-03-06 14:40:50 -0600

    logica para la ejecucion de frames

--- Proyect/P1ARCHIVOS/src/main/java/com/cresb/p1archivos/P1ARCHIVOS.java
+++ Proyect/P1ARCHIVOS/src/main/java/com/cresb/p1archivos/P1ARCHIVOS.java
@@ -1,9 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.cresb.p1archivos;
+
+import com.cresb.p1archivos.backend.models.Empleado;
+import com.cresb.p1archivos.backend.models.Rol;
+import com.cresb.p1archivos.frontend.LoginFrame;
+import com.cresb.p1archivos.frontend.pantallas.FrameAdmin;
+import com.cresb.p1archivos.frontend.pantallas.FrameBodega;
+import com.cresb.p1archivos.frontend.pantallas.FrameInventario;
+import com.cresb.p1archivos.frontend.pantallas.FrameVentas;
 
 /**
  *
@@ -12,6 +15,35 @@ package com.cresb.p1archivos;
 public class P1ARCHIVOS {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LoginFrame loginFrame = new LoginFrame(null, true);
+        loginFrame.setVisible(true);
+        //Recuperacion y limpieza del empleado recuperado
+        Empleado empleado = loginFrame.getEmpleado();
+        loginFrame.clearOption();
+        //Inicio segun el tipo de rol
+        switch (empleado.getRol().getId()) {
+            case Rol.VENDEDOR:
+                FrameVentas frameVentas = new FrameVentas(empleado);
+                frameVentas.setVisible(true);
+                frameVentas = null;
+                break;
+            case Rol.INVENTARIO:
+                FrameInventario frameInventario = new FrameInventario(empleado);
+                frameInventario.setVisible(true);
+                frameInventario = null;
+                break;
+            case Rol.BODEGA:
+                FrameBodega frameBodega = new FrameBodega(empleado);
+                frameBodega.setVisible(true);
+                frameBodega= null;
+                break;
+            case Rol.ADMIN:
+                FrameAdmin frameAdmin = new FrameAdmin(empleado);
+                frameAdmin.setVisible(true);
+                frameAdmin = null;
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
 }
