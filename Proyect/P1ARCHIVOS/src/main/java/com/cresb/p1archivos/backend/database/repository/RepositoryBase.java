1c9250f48c489b1904941be3211c8695b90e0465
cresb-49 <carlosbpac@gmail.com>
2023-03-11 00:11:04 -0600

    Cambio de usaurio de conexion de la base de datos

--- Proyect/P1ARCHIVOS/src/main/java/com/cresb/p1archivos/backend/database/repository/RepositoryBase.java
+++ Proyect/P1ARCHIVOS/src/main/java/com/cresb/p1archivos/backend/database/repository/RepositoryBase.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 public abstract class RepositoryBase {
     
     private static final String URL = "jdbc:postgresql://localhost:5432/electronichomes";
-    private static final String USER = "postgres";
+    private static final String USER = "admin";
     private static final String PASS = "201931012";
     
     protected static Connection GetConnection() throws SQLException{
