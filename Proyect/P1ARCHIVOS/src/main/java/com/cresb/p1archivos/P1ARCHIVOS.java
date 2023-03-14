24fb27524180ba28c3715e92741d77df1cee6cff
cresb-49 <carlosbpac@gmail.com>
2023-03-13 18:37:13 -0600

    manejo de inicio

--- Proyect/P1ARCHIVOS/src/main/java/com/cresb/p1archivos/P1ARCHIVOS.java
+++ Proyect/P1ARCHIVOS/src/main/java/com/cresb/p1archivos/P1ARCHIVOS.java
@@ -4,7 +4,7 @@ import com.cresb.p1archivos.backend.database.repository.SucursalRepository;
 import com.cresb.p1archivos.backend.models.Empleado;
 import com.cresb.p1archivos.backend.models.Rol;
 import com.cresb.p1archivos.frontend.LoginDialog;
-import com.cresb.p1archivos.frontend.pantallas.FrameAdmin;
+import com.cresb.p1archivos.frontend.pantallas.admin.FrameAdmin;
 import com.cresb.p1archivos.frontend.pantallas.bodega.FrameBodega;
 import com.cresb.p1archivos.frontend.pantallas.inventario.FrameInventario;
 import com.cresb.p1archivos.frontend.pantallas.ventas.FrameVentas;
@@ -30,54 +30,58 @@ public class P1ARCHIVOS {
         //Recuperacion y limpieza del empleado recuperado
         Empleado empleado = loginFrame.getEmpleado();
         loginFrame.clearOption();
-        //Inicio segun el tipo de rol
-        switch (empleado.getRol().getId()) {
-            case Rol.VENDEDOR:
-                //Recuperar la sucursal a la cual eesta enlazado el empleado
-                try {
-                    var sucursal = sucursalRepository.findByIdEmpleado(empleado.getNickname());
+        //Inicio segun el tipo de rol'
+        try {
+            switch (empleado.getRol().getId()) {
+                case Rol.VENDEDOR:
+                    //Recuperar la sucursal a la cual eesta enlazado el empleado
+                    try {
+                        var sucursal = sucursalRepository.findByIdEmpleado(empleado.getNickname());
 
-                    if(sucursal == null)
-                        throw new NullPointerException();
+                        if(sucursal == null)
+                            throw new NullPointerException();
 
-                    FrameVentas frameVentas = new FrameVentas(empleado,sucursal);
-                    frameVentas.setVisible(true);
-                    frameVentas = null;
-                }catch (SQLException ex){
-                    JOptionPane.showMessageDialog(null,"Error con la base de datos", "Error",JOptionPane.ERROR_MESSAGE);
-                }catch (NullPointerException ex){
-                    JOptionPane.showMessageDialog(null,"El empleado no esta enlazado con una sucursal", "Error",JOptionPane.ERROR_MESSAGE);
-                }
-                break;
-            case Rol.INVENTARIO:
-                //Recuperar la sucursal a la cual eesta enlazado el empleado
-                try {
-                    var sucursal = sucursalRepository.findByIdEmpleado(empleado.getNickname());
+                        FrameVentas frameVentas = new FrameVentas(empleado,sucursal);
+                        frameVentas.setVisible(true);
+                        frameVentas = null;
+                    }catch (SQLException ex){
+                        JOptionPane.showMessageDialog(null,"Error con la base de datos", "Error",JOptionPane.ERROR_MESSAGE);
+                    }catch (NullPointerException ex){
+                        JOptionPane.showMessageDialog(null,"El empleado no esta enlazado con una sucursal", "Error",JOptionPane.ERROR_MESSAGE);
+                    }
+                    break;
+                case Rol.INVENTARIO:
+                    //Recuperar la sucursal a la cual eesta enlazado el empleado
+                    try {
+                        var sucursal = sucursalRepository.findByIdEmpleado(empleado.getNickname());
 
-                    if(sucursal == null)
-                        throw new NullPointerException();
+                        if(sucursal == null)
+                            throw new NullPointerException();
 
-                    FrameInventario frameInventario = new FrameInventario(empleado,sucursal);
-                    frameInventario.setVisible(true);
-                    frameInventario = null;
-                }catch (SQLException ex){
-                    JOptionPane.showMessageDialog(null,"Error con la base de datos", "Error",JOptionPane.ERROR_MESSAGE);
-                }catch (NullPointerException ex){
-                    JOptionPane.showMessageDialog(null,"El empleado no esta enlazado con una sucursal", "Error",JOptionPane.ERROR_MESSAGE);
-                }
-                break;
-            case Rol.BODEGA:
-                FrameBodega frameBodega = new FrameBodega(empleado);
-                frameBodega.setVisible(true);
-                frameBodega= null;
-                break;
-            case Rol.ADMIN:
-                FrameAdmin frameAdmin = new FrameAdmin(empleado);
-                frameAdmin.setVisible(true);
-                frameAdmin = null;
-                break;
-            default:
-                System.out.println("Salida del programa :)");
+                        FrameInventario frameInventario = new FrameInventario(empleado,sucursal);
+                        frameInventario.setVisible(true);
+                        frameInventario = null;
+                    }catch (SQLException ex){
+                        JOptionPane.showMessageDialog(null,"Error con la base de datos", "Error",JOptionPane.ERROR_MESSAGE);
+                    }catch (NullPointerException ex){
+                        JOptionPane.showMessageDialog(null,"El empleado no esta enlazado con una sucursal", "Error",JOptionPane.ERROR_MESSAGE);
+                    }
+                    break;
+                case Rol.BODEGA:
+                    FrameBodega frameBodega = new FrameBodega(empleado);
+                    frameBodega.setVisible(true);
+                    frameBodega= null;
+                    break;
+                case Rol.ADMIN:
+                    FrameAdmin frameAdmin = new FrameAdmin(empleado);
+                    frameAdmin.setVisible(true);
+                    frameAdmin = null;
+                    break;
+                default:
+                    System.out.println("Salida del programa :)");
+            }
+        } catch (Exception ex) {
+            System.out.println("Salida del programa :)");
         }
     }
 }
